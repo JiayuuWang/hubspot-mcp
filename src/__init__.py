6a7ec4257281c86c347b7bc1f405a8b8479e625d
jacob-wang <jiayuw794@gmail.com>
2026-05-31 17:50:21 +0800

    Update for Dedalus deployment: add dotenv, fix dependencies, update license headers

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,2 @@
-from .types import *
-from .config import *
-from .tools import *
-from . import hubspot
+# Copyright (c) 2026 Dedalus Labs, Inc. and its contributors
+# SPDX-License-Identifier: MIT
--- src/hubspot/__init__.py
+++ src/hubspot/__init__.py
@@ -1,3 +1,7 @@
-from . import config
-from . import tools
-from . import types
+# Copyright (c) 2026 Dedalus Labs, Inc. and its contributors
+# SPDX-License-Identifier: MIT
+from . import config, tools, types
+from .config import hubspot
+from .tools import hubspot_tools
+
+__all__ = ["hubspot", "hubspot_tools"]
